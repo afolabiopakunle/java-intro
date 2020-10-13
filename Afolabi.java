cce28bf91f1c5aa175bd8712fd69e1a811d8899a
Afolabi Opakunle <opakunleafolabi@gmail.com>
2020-10-13 18:35:32 +0100

    redone strings

--- Afolabi.java
+++
@@ -1,5 +0,0 @@
-public class Afolabi {
-	public static void main(String[] args) {
-		System.out.println("Afolabi Himself")/* We dey here */;
-	}
-}
---
+++ DisplayName.java
@@ -0,0 +1,7 @@
+public class DisplayName {
+	
+	public static void main(String[] args) {
+	String name = "Afolabi Oluseun Opakunle";
+	System.out.println(name);
+	}
+}
