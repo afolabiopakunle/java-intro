a95e3c2caf6003ee5b27cea719b0fc7e48aab219
Afolabi Opakunle <opakunleafolabi@gmail.com>
2020-10-14 21:02:17 +0100

    Arrays test

---
+++ Arrays.java
@@ -0,0 +1,8 @@
+public class Arrays {
+    public static void main(String[] args) {
+        String[] people = {"Afolabi", "Desire", "Rilwan"};
+        for(String i : people) {
+            System.out.println(i);
+        }
+    }
+}
--- DisplayName.java
+++ DisplayName.java
@@ -1,7 +1,10 @@
 public class DisplayName {
 	
 	public static void main(String[] args) {
+	int age = 45;
 	String name = "Afolabi Oluseun Opakunle";
-	System.out.println(name);
+		System.out.println(age);
+		System.out.println(name);
+		System.out.println(name.length());
 	}
 }
