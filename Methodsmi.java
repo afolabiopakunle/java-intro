3e1aa12e539b1bf7e222a5e6c31a9534baa29747
Afolabi Opakunle <opakunleafolabi@gmail.com>
2020-10-14 22:56:49 +0100

    still learning

---
+++ Methodsmi.java
@@ -0,0 +1,9 @@
+public class Methodsmi {
+    static void printSth() {
+        System.out.println("Methods Printing");
+    }
+
+    public static void main(String[] args) {
+        printSth();
+    }
+}
